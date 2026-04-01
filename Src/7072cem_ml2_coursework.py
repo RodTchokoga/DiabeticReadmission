1b5668d8da632665da459f9e27210cae84e72eb4
Rodez <rtchokoga@gmail.com>
2026-04-01 16:48:25 +0100

    Fix import statement for pandas

--- Src/7072cem_ml2_coursework.py
+++ Src/7072cem_ml2_coursework.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1KHlHBunD9HyA5HsVBBZu3sbiAJjIFD4y
 """
 
-import pandas as 
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
